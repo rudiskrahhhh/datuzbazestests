6b66f9c4ce6261e66888ae86b8f37cd4f34c4dbe
rudiskrahhhh <rudis.kruskops@gmail.com>
2024-11-07 16:08:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sqlite3
 from flask import Flask, render_template, request, redirect
-from dati import iegut_skolotajus, pievienot_skolenu, pievienot_prieksmetu, pievienot_skolotaju, iegut_skolenus, iegut_prieksmetus, pievienot_atzimi
+from dati import iegut_skolotajus, pievienot_skolenu, pievienot_prieksmetu, pievienot_skolotaju, iegut_skolenus, iegut_prieksmetus
+from dati import pievienot_atzimi, iegut_atzimes, pievienot_skolotaju_prieksmetam, iegut_skolotaju_prieksmetus
+
 
 app = Flask(__name__)
 
@@ -12,11 +14,11 @@ def index():
     prieksmeti_no_db = iegut_prieksmetus()
         
     if request.method == "POST":
-        vards = request.form['name']
-        uzvards = request.form['lastname']
-        skolotajs_v = request.form['sk_name']
-        skolotajs_uzv = request.form['sk_lastname']
-        prieksmets = request.form['prieksmets']
+        vards = request.form['name'].capitalize()
+        uzvards = request.form['lastname'].capitalize()
+        skolotajs_v = request.form['sk_name'].capitalize()
+        skolotajs_uzv = request.form['sk_lastname'].capitalize()
+        prieksmets = request.form['prieksmets'].capitalize()
         if vards and uzvards:
             pievienot_skolenu(vards, uzvards)
         if skolotajs_uzv and skolotajs_v:
@@ -36,9 +38,11 @@ def pievienot():
     skolotaji = iegut_skolotajus()
     skoleni = iegut_skolenus()
     prieksmeti = iegut_prieksmetus()
+    atzimju_dati = iegut_atzimes()
+    skolotaju_prieksmeti = iegut_skolotaju_prieksmetus()
     # if request.method == "POST":
     #     print(request.form['skolotajs'])
-    return render_template("pievienot.html", skolotaji = skolotaji, skoleni=skoleni, prieksmeti=prieksmeti)
+    return render_template("pievienot.html", skolotaji = skolotaji, skoleni=skoleni, prieksmeti=prieksmeti, atzimes = atzimju_dati, skolotajuPrieksmeti = skolotaju_prieksmeti)
 
 @app.route("/pievienot/atzimi", methods=["POST"])
 def atzime():
@@ -48,7 +52,12 @@ def atzime():
     pievienot_atzimi(atzime, skolens, prieksmets)
     return redirect("/pievienot")
 
-
+@app.route("/pievienot/skolotaji", methods=["POST"])
+def skolotaji():
+    skolotajs = request.form["skolotajs"]
+    prieksmets = request.form["prieksmets"]
+    pievienot_skolotaju_prieksmetam(skolotajs, prieksmets)
+    return redirect("/pievienot")
 
 @app.route("/atzimes")
 def atzimes():
