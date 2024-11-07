5be1fa592f73cfd2fb02110ec989868fbc2a986c
rudiskrahhhh <rudis.kruskops@gmail.com>
2024-11-07 16:29:15 +0200

    Update dati.py

--- dati.py
+++ dati.py
@@ -16,6 +16,7 @@ def skolenu_tabulas_izveide():
     )
     conn.commit()
 
+
 def skolotaju_tabulas_izveide():
     cur = conn.cursor()
     cur.execute(
@@ -40,6 +41,38 @@ def prieksmetu_tabulas_izveide():
         """
     )
     conn.commit()
+
+def atzimju_tabulas_izveide():
+    cur = conn.cursor()
+    cur.execute("""
+CREATE TABLE atzimes(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    atzime INTEGER NOT NULL,
+    skolena_id INTEGER NOT NULL,
+    prieksmeta_id INTEGER NOT NULL,
+    FOREIGN KEY (skolena_id) REFERENCES skoleni(id),
+    FOREIGN KEY (prieksmeta_id) REFERENCES prieksmeti(id)
+                )
+    """)
+    conn.commit()
+
+def skolotaju_prieksmetu_tabulas_izveide():
+    cur = conn.cursor()
+    cur.execute("""
+CREATE TABLE skolotajuPrieksmeti(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    skolotaja_id INTEGER NOT NULL,
+    prieksmeta_id INTEGER NOT NULL,
+    FOREIGN KEY (skolotaja_id) REFERENCES skolotaji(id),
+    FOREIGN KEY (prieksmeta_id) REFERENCES prieksmeti(id)
+                )
+    """)
+    conn.commit()
+
+# skolotaju_prieksmetu_tabulas_izveide()
+
+# atzimju_tabulas_izveide()
+
 # skolotaju_tabulas_izveide()
 
 # skolenu_tabulas_izveide()
@@ -64,6 +97,7 @@ def pievienot_skolotaju(vards, uzvards):
     INSERT INTO skolotaji(vards, uzvards) VALUES("{vards}","{uzvards}")
     """
     )
+    conn.commit()
 
     print(vards, uzvards)
 
@@ -106,3 +140,63 @@ def iegut_prieksmetus():
     conn.commit()
     dati = cur.fetchall()
     return dati
+
+def pievienot_atzimi(atzime, skolens, prieksmets):
+    cur = conn.cursor()
+    cur.execute(
+    f"""
+    INSERT INTO atzimes(atzime, skolena_id, prieksmeta_id) VALUES("{atzime}","{skolens}","{prieksmets}")
+    """
+    )
+    conn.commit()
+
+def iegut_atzimes():
+    cur = conn.cursor()
+    cur.execute(
+        """SELECT vards, uzvards, nosaukums, atzime 
+        FROM 
+        (atzimes JOIN skoleni ON skoleni.id = atzimes.skolena_id)
+        JOIN prieksmeti ON prieksmeti.id = atzimes.prieksmeta_id
+        """
+    )
+    conn.commit()
+    dati = cur.fetchall()
+    return dati
+
+def pievienot_skolotaju_prieksmetam(skolotajs, prieksmets):
+    cur = conn.cursor()
+    cur.execute(
+    f"""
+    INSERT INTO skolotajuPrieksmeti(skolotaja_id, prieksmeta_id) VALUES("{skolotajs}","{prieksmets}")
+    """
+    )
+    conn.commit()
+
+def iegut_skolotaju_prieksmetus():
+    cur = conn.cursor()
+    cur.execute(
+        """SELECT vards, uzvards, nosaukums 
+        FROM 
+        (skolotajuPrieksmeti JOIN skolotaji ON skolotaji.id = skolotajuPrieksmeti.skolotaja_id)
+        JOIN prieksmeti ON prieksmeti.id = skolotajuPrieksmeti.prieksmeta_id
+        """
+    )
+    conn.commit()
+    dati = cur.fetchall()
+    return dati
+
+def iegut_videjas_atzimes():
+    cur = conn.cursor()
+    cur.execute(
+        """SELECT skoleni.vards, skoleni.uzvards, prieksmeti.nosaukums, AVG(atzimes.atzime), skoleni.id, prieksmeti.id
+        FROM (skoleni  LEFT JOIN atzimes ON skoleni.id = atzimes.skolena_id) 
+             LEFT JOIN prieksmeti ON prieksmeti.id =atzimes.prieksmeta_id
+        GROUP BY prieksmeti.id, skoleni.id
+        ORDER BY 
+        """
+    )
+    conn.commit()
+    dati = cur.fetchall()
+    return dati
+
+def dzest_skolenu(id):
